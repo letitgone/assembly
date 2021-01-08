1eacd6217f180aeabc151cd294841e1fe0b99c15
ZhangGJ <48301286+letitgone@users.noreply.github.com>
2021-01-08 08:12:15 +0800

    loop

---
+++ base/p3.asm
@@ -0,0 +1,15 @@
+assume cs:code
+code segment
+    mov ax, offffh
+    mov ds, ax
+    mov bx, 6
+    mov al, [bx]
+    mov ah, 0
+    mov dx, 0
+    mov cx, 3
+    s:add dx, ax
+    loop s
+    mov ax, 4c00h
+    int 21h
+code ends
+end
---
+++ base/p4.asm
@@ -0,0 +1,15 @@
+assume cs:code
+code segment
+    mov ax, -ffffh
+    mov dx, ax
+    mov bx, 6
+    mov al, [bx]
+    mov ah, 0
+    mov dx, 0
+    mov cx, 123
+    s:add dx, ax
+    loop s
+    mov ax, 4c00h
+    int 21h
+code ends
+end
